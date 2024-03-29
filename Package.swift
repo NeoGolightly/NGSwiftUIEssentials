cfc85b9ef8f3f19a8d8f8595edfa84d0ca69d72d
at//design <mail@at-world.de>
2024-03-29 12:15:47 +0100

    Enabled Strict Concurrency in package

--- Package.swift
+++ Package.swift
@@ -4,21 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "NGSwiftUIEssentials",
-    platforms: [.iOS(.v17), .macOS(.v14), .watchOS(.v10), .tvOS(.v17), .visionOS(.v1)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "NGSwiftUIEssentials",
-            targets: ["NGSwiftUIEssentials"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NGSwiftUIEssentials"),
-        .testTarget(
-            name: "NGSwiftUIEssentialsTests",
-            dependencies: ["NGSwiftUIEssentials"]),
-    ]
+  name: "NGSwiftUIEssentials",
+  platforms: [.iOS(.v17), .macOS(.v14), .watchOS(.v10), .tvOS(.v17), .visionOS(.v1)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "NGSwiftUIEssentials",
+      targets: ["NGSwiftUIEssentials"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "NGSwiftUIEssentials",
+      swiftSettings: [
+        .enableExperimentalFeature("StrictConcurrency")
+      ]
+    ),
+    .testTarget(
+      name: "NGSwiftUIEssentialsTests",
+      dependencies: ["NGSwiftUIEssentials"]),
+  ]
 )
